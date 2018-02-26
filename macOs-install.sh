19d96b157d6c420784d5a0c4d86e6efd0b0c99f8
Victor Piolin <victor.piolin@gmail.com>
2018-02-26 22:46:00 +0100

    Cask + bashprofile

--- macOs-install.sh
+++ macOs-install.sh
@@ -1,33 +1,47 @@
 #!/bin/sh
 
-echo "Installing brew..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+mv bash_profile.txt /Users/vico/.bash_profile
 
-echo "Installing brew cask..."
-brew tap caskroom/cask
+#Default MacOs Configuration
+chflags nohidden ~/Library
+defaults write com.apple.finder AppleShowAllFiles YES
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
 
-#Xcode
-xcode-select --install
+# echo "Installing brew..."
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#
+# Make sure Brew has permissions
+# brew doctor
+#
+# # Update Brew
+# brew update
 
-#Mac AppleStore-cli
-brew install mas
-mas signin vicomacbook@icloud.com
-
-#Outil
-brew install git
-brew cask install iterm2
-brew cask install docker
-brew install visual-studio-code
-
-#Communication
-brew cask install slack
-
-#Web Tools
-brew cask install google-chrome
-
-#Other
-brew cask vlc
-
-#Configuration MacOs
-
-touch .bash_profile
+# echo "Installing brew cask..."
+# brew tap caskroom/cask
+#
+# #Xcode
+# xcode-select --install
+#
+# #Mac AppleStore-cli
+# brew install mas
+# mas signin vicomacbook@icloud.com
+#
+# #Outil
+# brew install git
+# brew cask install iterm2
+# brew cask install docker
+# brew install visual-studio-code
+#
+# #Communication
+# brew cask install slack
+#
+# #Web Tools
+# brew cask install google-chrome
+#
+# #Other
+# brew cask vlc
+#
+# #Configuration MacOs
+#
+# touch .bash_profile
